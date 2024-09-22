76ced59327431bbeec56631ed07e098276f0d163
Shivanna <shivanna.hadapad@gmail.com>
2024-09-22 11:08:03 +0530

    Added orderpage

--- src/main/java/com/qa/openakrt/pages/OrderPage.java
+++ src/main/java/com/qa/openakrt/pages/OrderPage.java
@@ -1,2 +1,7 @@
-package com.qa.openakrt.pages;public class OrderPage {
+package com.qa.openakrt.pages;
+
+public class OrderPage {
+    public static void main(String[] args) {
+        int i = 20;
+    }
 }
