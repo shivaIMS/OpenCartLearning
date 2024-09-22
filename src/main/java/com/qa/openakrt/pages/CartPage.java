e824f2b2e5a796a4c06bac443fa7b16e0d666fb6
Shivanna <shivanna.hadapad@gmail.com>
2024-09-22 17:36:15 +0530

    updated CartPage

--- src/main/java/com/qa/openakrt/pages/CartPage.java
+++ src/main/java/com/qa/openakrt/pages/CartPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 
 public class CartPage {
     int i = 10;
-    private By name = By.id("test");
+    private By name = By.id("testing");
     public void getName(){
         System.out.println("name");
     }
